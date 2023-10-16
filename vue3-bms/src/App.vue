e71fda92eb3793979830c0ee4c8ee67cf880263f
chike95 <148038535+chike95@users.noreply.github.com>
2023-10-16 15:33:49 +0800

    elementPlus  and layout

--- vue3-bms/src/App.vue
+++ vue3-bms/src/App.vue
@@ -1,13 +1,50 @@
 <template>
-  <HelloWorld msg="Vite + Vue" />
-  <router-link to="/">Go to home</router-link>
-  <router-link to="/about">Go to About</router-link>
-  <router-view />
+  <div class="common-layout">
+    <el-container>
+      <el-aside width="200px">
+        <Sidebar></Sidebar>
+      </el-aside>
+
+      <el-container>
+        <el-header>
+          <CommonHeader></CommonHeader>
+        </el-header>
+        <el-main>
+          <router-view></router-view>
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
 </template>
 
 <script setup>
-
+import CommonHeader from '@/components/CommonHeader.vue';
+import Sidebar from '@/components/Sidebar.vue';
 
 </script>
 
-<style scoped></style>
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+}
+
+body,
+html {
+  margin: 0;
+  padding: 0;
+}
+
+.el-aside {
+  height: 100vh;
+  background-color: #be1313;
+}
+
+.el-header {
+  height: 76px;
+  background-color: #17ce63;
+}
+
+.el-main {
+  background-color: #7b7ac9;
+}</style>
